9e9f6916409eb4f06e174e19685e1ccdf05bb813
tkdgheee <sangho135790@gmail.com>
2025-06-26 17:11:11 +0900

    fix: rebase과정 중 버튼 미반영된 코드 반영(#66)
    - rebase과정 중 버튼 미반영된 코드 반영

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,35 +1,35 @@
-import { BUTTON_VARIANTS, DEFAULT_BUTTON_VARIANT } from "@constants/button";
-import { cn } from "@utils/cn"
-
-
-type ButtonVariant = keyof typeof BUTTON_VARIANTS
-
+import React from 'react'
 
 type ButtonProps = {
-  children: React.ReactNode;
-  onClick?: () => void;
-  disabled?: boolean;
-  variant?: ButtonVariant; 
-  className?: string;
-};
+  children: React.ReactNode
+  onClick?: () => void
+  disabled?: boolean
+  variant?: 'register' | 'delete' | 'cancel'
+  className?: string
+}
+
+const baseStyle = 'px-4 py-2 rounded font-semibold focus:outline-none '
+
+const variantStyles = {
+  register: 'bg-blue-500 hover:bg-blue-600 text-white',
+  delete: 'bg-red-500 hover:bg-red-600 text-white',
+  cancel: 'bg-gray-300 hover:bg-gray-400 text-black',
+}
 
 const Button: React.FC<ButtonProps> = ({
   children,
   onClick,
   disabled = false,
-  variant = DEFAULT_BUTTON_VARIANT, 
-  className
-}) => { 
+  variant = 'register',
+  className = '',
+}) => {
   return (
     <button
       onClick={onClick}
       disabled={disabled}
-      className={cn(
-        'px-4 py-2 font-semibold w-[55px] h-[36px]',
-        BUTTON_VARIANTS[variant],
-        {' cursor-not-allowed' : disabled},
-        className
-      )}
+      className={`${baseStyle} ${variantStyles[variant]} ${
+        disabled ? 'cursor-not-allowed opacity-50' : ''
+      } ${className}`}
     >
       {children}
     </button>
