829d474358dd61bcb86f48c701ada5a6847a0347
tkdgheee <sangho135790@gmail.com>
2025-06-26 17:20:47 +0900

    fix: rebase과정 중 버튼 미반영된 코드 반영(#66)
    - rebase과정 중 버튼 button.ts에 미반영된 코드 반영

--- src/constants/button.ts
+++ src/constants/button.ts
@@ -1,8 +1,9 @@
 export const BUTTON_VARIANTS = {
-  VARIANT1: 'bg-[#522193] text-white',
-  VARIANT2: 'bg-[#86838B] text-white',
-  VARIANT3: 'bg-[#FFFFFF] text-black border border-gray-300 rounded-[3px]',
-  VARIANT4: 'bg-[#CC0A0A] text-white',
+  REGISTER: 'bg-purple-700 hover:bg-purple-800 text-white top-5 left-5',
+  GRAY_DELETE: 'bg-gray-500 hover:bg-gray-600 text-white top-[76px] left-5',
+  CANCEL:
+    'bg-white text-black border border-gray-300 top-[132px] left-5 rounded-[3px]',
+  DELETE: 'bg-red-500 hover:bg-red-600 text-white top-[188px] left-5',
 } as const
 
-export const DEFAULT_BUTTON_VARIANT = 'VARIANT1'
+export const DEFAULT_BUTTON_VARIANT = 'REGISTER'
